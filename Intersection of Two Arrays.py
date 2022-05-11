0f5d17606cfb1bde8a756b54dd1b9aefae894ac2
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-05-11 10:36:05 -0700

    Add files via upload

--- Intersection of Two Arrays.py
+++ Intersection of Two Arrays.py
@@ -25,3 +25,21 @@ class Solution:
         #         map[j] = 0
 
         # return res
+
+        # Solution 4 sort two lists then use two pointer to search common elements
+        # res = []
+        # nums1.sort()
+        # nums2.sort()
+        # i = j = 0
+        # while (i < len(nums1) and j < len(nums2)):
+        #     if nums1[i] > nums2[j]:
+        #         j += 1
+        #     elif nums1[i] < nums2[j]:
+        #         i += 1
+        #     else:
+        #         if not (len(res) and nums1[i] == res[len(res)-1]):
+        #             res.append(nums1[i])
+        #         i += 1
+        #         j += 1
+        
+        # return res

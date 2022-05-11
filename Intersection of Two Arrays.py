151208dca6d3e3f70fe9290e12222157acf332de
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-05-11 10:33:33 -0700

    Add files via upload

--- Intersection of Two Arrays.py
+++ Intersection of Two Arrays.py
@@ -6,4 +6,10 @@ class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
         return list(set(nums1) & set(nums2)) # Python set operation
 
-        # Solution 2
+        # Solution 2 brute force
+        # res = []
+        # for i in nums1:
+        #     if i not in res and i in nums2:
+        #         res.append(i)
+
+        # return res
